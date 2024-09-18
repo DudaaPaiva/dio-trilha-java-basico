493b84b5088a7129006d47125f06f917e452f1ca
Eduarda Paiva <eduarda.a.paiva@gmail.com>
2024-09-17 23:01:43 -0300

    criando uma tv smart para exercicios de metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        System.out.println("Canal atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual : " + smartTv.canal);
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        System.out.println("Canal atual : " + smartTv.canal);
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        smartTv.ligar();// chama método ligar
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();// chama método desligar
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+    }
+}
