02253ab0b27f2d91da8f8879d46b436c2d1cb575
Eduarda Paiva <eduarda.a.paiva@gmail.com>
2024-10-03 12:44:18 -0300

    aula sobre for

---
+++ controle-de-fluxo/src/ExemploFor.java
@@ -0,0 +1,11 @@
+public class ExemploFor {
+  public static void main(String[] args) {
+    for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {// primeiro a variável carneirinhos do tipo inteiro
+                                                                    // se inicializa com o valor 1, após o ponto ; é
+                                                                    // verificado se o número é menor ou igual a 20,
+                                                                    // caso seja o valor será incrementado em +1 até que
+                                                                    // a condição se torne falsa
+      System.out.println("Contando carneirinhos : " + carneirinhos);
+    }
+  }
+}
