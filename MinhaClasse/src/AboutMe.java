23c34e7b7588a10e23bbc0e92f09734ff2bea955
Eduarda Paiva <eduarda.a.paiva@gmail.com>
2024-10-04 12:35:13 -0300

    aula sobre tratamento de execeções

--- MinhaClasse/src/AboutMe.java
+++ MinhaClasse/src/AboutMe.java
@@ -1,28 +1,32 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class AboutMe {
     public static void main(String[] args) throws Exception {
-        // criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {// o bloco que captura precisa conter todos os outros blocos que serão testados.
+             // criando o objeto scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.next();
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu sobrenome: ");
-        String sobrenome = scanner.next();
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade: ");
-        int idade = scanner.nextInt();
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
 
-        scanner.close();
+            scanner.close();
 
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " cm ");
-
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + " cm ");
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser númericos");
+        }
     }
 }
